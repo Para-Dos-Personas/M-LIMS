65594c70598cde90fc2858f644c1ee47f86e4abe
AllenPrabu <allenthiagarajprabu@gmail.com>
2025-09-07 18:20:10 +0530

    Testing WarehouseAPI

--- backend/routes/dashboardRoutes.js
+++ backend/routes/dashboardRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const { Component, ComponentLog } = require('../models');
-const { authenticateToken } = require('../middleware/auth');
+const  authenticateToken  = require('../middleware/auth');
 const { Op } = require('sequelize');
 const sequelize = require('../config/db');
 
